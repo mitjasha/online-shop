54db3cf959dd34569a6d5ebd0bc8dc11e3dbe1ef
mitjasha <mitjasha@gmail.com>
2022-12-19 00:51:24 +0300

    feat: add footer template slytes

--- src/components/containers/Footer/Footer.tsx
+++ src/components/containers/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className="footer">
       <div>Footer</div>
     </footer>
   );
