af7a47a522757896623a2fd8c84ea353909a5b7e
mitjasha <mitjasha@gmail.com>
2022-12-19 00:31:37 +0300

    feat: product card page template

---
+++ src/components/containers/ProductCardPage/ProductCardPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProductCardPage: React.FC = () => {
+  return (
+    <div>
+      <div>ProductCardPage</div>
+    </div>
+  );
+};
+export default ProductCardPage;
