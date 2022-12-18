224cc97e38fb785d27a55096211744d3144455c4
mitjasha <mitjasha@gmail.com>
2022-12-19 00:44:36 +0300

    feat: add header template style

--- src/components/containers/Header/Header.tsx
+++ src/components/containers/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Header.css";
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <header className="header">
       <div>Header</div>
     </header>
   );
