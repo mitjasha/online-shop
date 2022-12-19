63fa3c0c06ca81d90ee6c1884372dd8137d8cf64
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-19 18:11:17 +0300

    feat: add logo and navigation

--- src/components/containers/Header/Header.tsx
+++ src/components/containers/Header/Header.tsx
@@ -4,7 +4,17 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div>Header</div>
+      <div className="logo-search">
+        <img src="../../../assets/logo.svg" alt="online store logo" />
+      </div>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="list__item">HOME</li>
+          <li className="list__item">CATALOGUE</li>
+          <li className="list__item">CONTACTS</li>
+          <li className="list__item">CART</li>
+        </ul>
+      </nav>
     </header>
   );
 };
