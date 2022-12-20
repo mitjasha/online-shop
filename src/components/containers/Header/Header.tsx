17230d53fb4dedfdfbeb8ac506e17bcafc15b2d5
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-20 22:20:04 +0300

    feat: add icons to header

--- src/components/containers/Header/Header.tsx
+++ src/components/containers/Header/Header.tsx
@@ -4,10 +4,43 @@ import "./Header.css";
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="logo-search">
-        <img src="../../../assets/logo.svg" alt="online store logo" />
+      <div className="header__wrapper">
+        <img
+          // eslint-disable-next-line global-require
+          src={require("../../../assets/img/logo.svg")}
+          alt="online store logo"
+        />
+        <div className="wrapper__items">
+          <div className="search">
+            <img
+              // eslint-disable-next-line global-require
+              src={require("../../../assets/img/search-icon.png")}
+              alt="search"
+              className="search-button"
+            />
+            <input
+              type="search"
+              className="search-input"
+              id="site-search"
+              placeholder="Search"
+            />
+          </div>
+          <div className="icons">
+            <div>Total: $0.00</div>
+            <img // eslint-disable-next-line global-require
+              src={require("../../../assets/img/heart-icon.png")}
+              alt="heart"
+              className="heart-button"
+            />
+            <img // eslint-disable-next-line global-require
+              src={require("../../../assets/img/cart-icon.png")}
+              alt="cart"
+              className="cart-button"
+            />
+          </div>
+        </div>
       </div>
-      <nav className="nav">
+      <nav className="header__nav">
         <ul className="nav__list">
           <li className="list__item">HOME</li>
           <li className="list__item">CATALOGUE</li>
