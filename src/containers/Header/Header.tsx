4fbf7ee82a5dc471cfb21b659d0d98b475d54b2c
mitjasha <mitjasha@gmail.com>
2022-12-22 16:49:52 +0300

    feat: add main menu navigation component

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -3,7 +3,8 @@ import "./Header.scss";
 import heartIcon from "../../assets/img/svg/heart-icon.svg";
 import cartIcon from "../../assets/img/svg/cart-icon.svg";
 import logo from "../../assets/img/svg/logo.svg";
-import Search from "../../components/search/search";
+import SearchComponent from "../../components/SearchComponent/SearchComponent";
+import MainMenuNavigation from "../../components/MainMenuNavigation/MainMenuNavigation";
 
 const Header: React.FC = () => {
   return (
@@ -11,7 +12,7 @@ const Header: React.FC = () => {
       <div className="header__wrapper">
         <img src={logo} alt="online store logo" />
         <div className="wrapper__items">
-          <Search />
+          <SearchComponent />
           <div className="shopping-info">
             <div className="total-sum">Total: $0.00</div>
             <img src={heartIcon} alt="favorities" className="favorities" />
@@ -19,14 +20,7 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="list__item">HOME</li>
-          <li className="list__item">CATALOGUE</li>
-          <li className="list__item">CONTACTS</li>
-          <li className="list__item">CART</li>
-        </ul>
-      </nav>
+      <MainMenuNavigation />
     </header>
   );
 };
