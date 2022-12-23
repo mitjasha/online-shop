d80964bb7703a89cf4c07963a0c9a5ce217f8b1a
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-23 13:39:33 +0300

    feat: change crlf to lf at module.d.ts

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Footer.scss";
+import logo from "../../assets/img/svg/logo.svg";
+import MainMenuNavigation from "../../components/MainMenuNavigation/MainMenuNavigation";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div>Footer</div>
+    <footer className="footer container">
+      <MainMenuNavigation />
+      <div className="footer__wrapper">
+        <img src={logo} alt="online store logo" />
+      </div>
     </footer>
   );
 };
