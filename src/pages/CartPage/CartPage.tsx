ded80727f73ba6c1bd29f94f5c8a3bcc62ec5ba2
mitjasha <mitjasha@gmail.com>
2022-12-24 14:49:13 +0300

    refactor: add span tags for text

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -7,7 +7,9 @@ const CartPage: React.FC = () => {
   return (
     <div className="cart-page">
       <div className="container">
-        <div className="cart-page__title">Shopping Cart ({0})</div>
+        <div className="cart-page__title">
+          <span className="cart-page__title-text">Shopping Cart ({0})</span>
+        </div>
         <div className="purchases">
           <div className="purchases__table-container">
             <div className="purchases__wrapper">
@@ -22,16 +24,20 @@ const CartPage: React.FC = () => {
                 placeholder="Coupon code"
               />
               <Button className="purchases__table-promo-button">
-                Apply coupon
+                <span>Apply coupon</span>
               </Button>
             </div>
           </div>
           <div className="purchases__total-container">
-            <div className="purchases__total-header">Cart totals</div>
+            <div className="purchases__total-header">
+              <span>Cart totals</span>
+            </div>
             <div className="purchases__total-body">
-              <div className="purchases__total-value">Total ${90.0}</div>
+              <div className="purchases__total-value">
+                <span>Total ${90.0}</span>
+              </div>
               <Button className="purchases__total-button">
-                Place an order
+                <span>Place an order</span>
               </Button>
             </div>
           </div>
