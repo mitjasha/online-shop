ef1eeb76fe2de833f8396027eb8be31e77a7282c
mitjasha <mitjasha@gmail.com>
2022-12-24 13:23:09 +0300

    feat: add purchases table row template

---
+++ src/components/PurchasesTableRow/PurchasesTableRow.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PurchasesTableRow.scss";
+
+const PurchasesTableRow: React.FC = () => {
+  return <div className="purchases__table-row">PurchasesTableRow</div>;
+};
+
+export default PurchasesTableRow;
