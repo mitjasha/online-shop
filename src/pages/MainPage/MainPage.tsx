270a44a9e83c40b4565a4cd4d329d8014fa0ddaa
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-25 19:51:45 +0300

    feat: add categories images to assets, add categories section, wrapper and img tags

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./MainPage.scss";
+import redWine from "../../assets/img/jpg/red-main.jpg";
+import whiteWine from "../../assets/img/jpg/white-main.jpg";
+import sparklingWine from "../../assets/img/jpg/sparkling-main.jpg";
+import roseWine from "../../assets/img/jpg/rose-main.jpg";
 
 const MainPage: React.FC = () => {
   return (
@@ -18,6 +22,28 @@ const MainPage: React.FC = () => {
             <button type="button" className="preview__button">
               CART
             </button>
+          </div>
+        </section>
+        <section className="categories">
+          <div className="categories__wrapper">
+            <div>
+              <img src={redWine} alt="red wine" className="categories__image" />
+              <img
+                src={whiteWine}
+                alt="white wine"
+                className="categories__image"
+              />
+              <img
+                src={sparklingWine}
+                alt="sparkling wine"
+                className="categories__image"
+              />
+              <img
+                src={roseWine}
+                alt="rose wine"
+                className="categories__image"
+              />
+            </div>
           </div>
         </section>
       </div>
