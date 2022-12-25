ac7ea163921f5337fdb47a628a8fdf2ca8146fc9
mitjasha <mitjasha@gmail.com>
2022-12-25 21:17:46 +0300

    refactor: change structure to table

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../../components/Buttons/Button/Button";
-import PurchasesTableRow from "../../components/PurchasesTableRow/PurchasesTableRow";
+// import PurchasesTableRow from "../../components/PurchasesTableRow/PurchasesTableRow";
 import "./CartPage.scss";
 
 const CartPage: React.FC = () => {
@@ -11,36 +11,68 @@ const CartPage: React.FC = () => {
           <span className="cart-page__title-text">Shopping Cart ({0})</span>
         </div>
         <div className="purchases">
-          <div className="purchases__table-container">
-            <div className="purchases__wrapper">
-              <div className="purchases__table-header"> </div>
-              <PurchasesTableRow />
-              <PurchasesTableRow />
-            </div>
-            <div className="purchases__table-footer">
-              <input
-                className="purchases__table-promo-input"
-                type="text"
-                placeholder="Coupon code"
-              />
-              <Button className="purchases__table-promo-button">
-                <span>Apply coupon</span>
-              </Button>
-            </div>
-          </div>
-          <div className="purchases__total-container">
-            <div className="purchases__total-header">
-              <span>Cart totals</span>
-            </div>
-            <div className="purchases__total-body">
-              <div className="purchases__total-value">
-                <span>Total ${90.0}</span>
-              </div>
-              <Button className="purchases__total-button">
-                <span>Place an order</span>
-              </Button>
-            </div>
-          </div>
+          <table className="purchases-table">
+            <thead className="purchases-table__header">
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Subtotal</th>
+              </tr>
+            </thead>
+            <tbody className="purchases-table__body">
+              <tr className="purchases-table__row">
+                <td colSpan={4}> </td>
+              </tr>
+              <tr className="purchases-table__row">
+                <td colSpan={4}> </td>
+              </tr>
+              <tr className="purchases-table__row">
+                <td colSpan={4}> </td>
+              </tr>
+              <tr className="purchases-table__row">
+                <td colSpan={4}> </td>
+              </tr>
+              <tr className="purchases-table__row">
+                <td colSpan={4}> </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td className="purchases-table__promo">
+                  <input
+                    className="purchases-table__promo-input"
+                    type="text"
+                    placeholder="Coupon code"
+                  />
+                  <Button className="purchases-table__promo-button">
+                    <span>Apply coupon</span>
+                  </Button>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <table className="total-table">
+            <thead className="total-table__header">
+              <tr>
+                <th className="total-table__header-title">Cart totals</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="total-table__total">
+                <td className="total-table__total-text">
+                  Total <span className="total-table__total-value">$90.00</span>
+                </td>
+              </tr>
+              <tr className="total-table__submit">
+                <td>
+                  <Button className="total-table__submit-button">
+                    <span>Place an order</span>
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
