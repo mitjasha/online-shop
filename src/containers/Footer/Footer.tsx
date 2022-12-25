bb7b081ce82d889277adad28c1729a004555f0e0
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-25 15:25:24 +0300

    feat: add footer and header containers, change container class styles, add 100% width to header and footer classes

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -8,36 +8,38 @@ import fbIcon from "../../assets/img/svg/facebook-icon.svg";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer container">
-      <MainMenuNavigation />
-      <div className="footer__info">
-        <div className="logo-wrapper">
-          <img src={logo} alt="online store logo" />
-        </div>
-        <div className="phone-wrapper">
-          <div>+375 (25) 123-45-67</div>
-        </div>
-        <div className="media-wrapper">
-          <a
-            href="https://www.youtube.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={youtubeIcon} alt="youtube" />
-          </a>
-          <a href="http://vk.com/" target="_blank" rel="noopener noreferrer">
-            <img src={vkIcon} alt="vkontakte" />
-          </a>
-          <a
-            href="https://www.facebook.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={fbIcon} alt="facebook" />
-          </a>
-        </div>
-        <div className="athors-wrapper">
-          <div>© LITTLE BUMBLEBEES</div>
+    <footer className="footer">
+      <div className="container">
+        <MainMenuNavigation />
+        <div className="footer__info">
+          <div className="logo-wrapper">
+            <img src={logo} alt="online store logo" />
+          </div>
+          <div className="phone-wrapper">
+            <div>+375 (25) 123-45-67</div>
+          </div>
+          <div className="media-wrapper">
+            <a
+              href="https://www.youtube.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={youtubeIcon} alt="youtube" />
+            </a>
+            <a href="http://vk.com/" target="_blank" rel="noopener noreferrer">
+              <img src={vkIcon} alt="vkontakte" />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={fbIcon} alt="facebook" />
+            </a>
+          </div>
+          <div className="athors-wrapper">
+            <div>© LITTLE BUMBLEBEES</div>
+          </div>
         </div>
       </div>
     </footer>
