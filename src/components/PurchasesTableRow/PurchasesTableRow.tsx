d66cb8070c0f359f28d2f28d771edafe868fe194
mitjasha <mitjasha@gmail.com>
2022-12-26 00:47:29 +0300

    feat: add data

--- src/components/PurchasesTableRow/PurchasesTableRow.tsx
+++ src/components/PurchasesTableRow/PurchasesTableRow.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { WineInfo } from "../../utils/helpers/interfaces";
+import QuantityInput from "../QuantityInput/QuantityInput";
 import "./PurchasesTableRow.scss";
 
-const PurchasesTableRow: React.FC = () => {
-  return <div className="purchases__table-row">PurchasesTableRow</div>;
+const PurchasesTableRow: React.FC<WineInfo> = ({ title, image2, price }) => {
+  return (
+    <tr className="table-row">
+      <td className="">
+        <img src={image2} alt="wine pic" />
+      </td>
+      <td>
+        <p>{title}</p>
+      </td>
+      <td>
+        <p>{price}</p>
+      </td>
+      <td>
+        <QuantityInput />
+      </td>
+      <td>
+        <p>{price}</p>
+      </td>
+    </tr>
+  );
 };
 
 export default PurchasesTableRow;
