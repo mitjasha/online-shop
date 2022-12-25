87c2d4cd91346fecc9fe6a20ae7dcea472c4a085
mitjasha <mitjasha@gmail.com>
2022-12-26 00:48:13 +0300

    feat: add data and interfaces

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "../../components/Buttons/Button/Button";
-// import PurchasesTableRow from "../../components/PurchasesTableRow/PurchasesTableRow";
+import PurchasesTableRow from "../../components/PurchasesTableRow/PurchasesTableRow";
 import "./CartPage.scss";
+import data from "../../assets/data/data.json";
 
 const CartPage: React.FC = () => {
   return (
@@ -14,28 +15,33 @@ const CartPage: React.FC = () => {
           <table className="purchases-table">
             <thead className="purchases-table__header">
               <tr>
-                <th>Product</th>
+                <th colSpan={2}>Product</th>
                 <th>Price</th>
                 <th>Quantity</th>
                 <th>Subtotal</th>
               </tr>
             </thead>
             <tbody className="purchases-table__body">
-              <tr className="purchases-table__row">
-                <td colSpan={4}> </td>
-              </tr>
-              <tr className="purchases-table__row">
-                <td colSpan={4}> </td>
-              </tr>
-              <tr className="purchases-table__row">
-                <td colSpan={4}> </td>
-              </tr>
-              <tr className="purchases-table__row">
-                <td colSpan={4}> </td>
-              </tr>
-              <tr className="purchases-table__row">
-                <td colSpan={4}> </td>
-              </tr>
+              <PurchasesTableRow
+                title={data.goods[0].title}
+                image2={data.goods[0].image2}
+                price={data.goods[0].price}
+              />
+              <PurchasesTableRow
+                title={data.goods[1].title}
+                image2={data.goods[1].image2}
+                price={data.goods[1].price}
+              />
+              <PurchasesTableRow
+                title={data.goods[2].title}
+                image2={data.goods[2].image2}
+                price={data.goods[2].price}
+              />
+              <PurchasesTableRow
+                title={data.goods[3].title}
+                image2={data.goods[3].image2}
+                price={data.goods[3].price}
+              />
             </tbody>
             <tfoot>
               <tr>
