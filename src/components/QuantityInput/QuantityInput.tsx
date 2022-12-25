e1ee02ede9d7e81591845cf34f16ab239cc866c9
mitjasha <mitjasha@gmail.com>
2022-12-26 00:44:25 +0300

    feat: add quantity input component

---
+++ src/components/QuantityInput/QuantityInput.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./QuantityInput.scss";
+
+const QuantityInput: React.FC = () => {
+  const increaseQuantity = () => {
+    const value: HTMLInputElement | null =
+      document.querySelector(".quantity__input");
+    if (value) {
+      value.stepUp();
+    }
+  };
+
+  const decreaseQuantity = () => {
+    const value: HTMLInputElement | null =
+      document.querySelector(".quantity__input");
+    if (value) {
+      value.stepDown();
+    }
+  };
+
+  return (
+    <div className="quantity">
+      <button
+        type="button"
+        onClick={decreaseQuantity}
+        className="quantity__minus"
+      >
+        -
+      </button>
+      <input
+        className="quantity__input"
+        min="0"
+        name="quantity"
+        value="1"
+        type="number"
+      />
+      <button
+        type="button"
+        onClick={increaseQuantity}
+        className="quantity__plus"
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default QuantityInput;
