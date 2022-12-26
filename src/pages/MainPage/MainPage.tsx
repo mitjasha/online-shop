67a7cf78a42b9117359385cdd93fe8f5aec73757
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-26 15:35:22 +0300

    fix: remove container div at main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -8,45 +8,39 @@ import roseWine from "../../assets/img/jpg/rose-main.jpg";
 const MainPage: React.FC = () => {
   return (
     <main className="main">
-      <div className="container">
-        <section className="preview">
-          <h1 className="preview__quote">
-            I drink wine and
-            <br />I know things
-          </h1>
-          <p className="quote-author">Tyrion Lannister</p>
-          <div className="preview__buttons">
-            <button type="button" className="preview__button">
-              CATALOGUE
-            </button>
-            <button type="button" className="preview__button">
-              CART
-            </button>
+      <section className="preview">
+        <h1 className="preview__quote">
+          I drink wine and
+          <br />I know things
+        </h1>
+        <p className="quote-author">Tyrion Lannister</p>
+        <div className="preview__buttons">
+          <button type="button" className="preview__button">
+            CATALOGUE
+          </button>
+          <button type="button" className="preview__button">
+            CART
+          </button>
+        </div>
+      </section>
+      <section className="categories">
+        <div className="categories__wrapper">
+          <div>
+            <img src={redWine} alt="red wine" className="categories__image" />
+            <img
+              src={whiteWine}
+              alt="white wine"
+              className="categories__image"
+            />
+            <img
+              src={sparklingWine}
+              alt="sparkling wine"
+              className="categories__image"
+            />
+            <img src={roseWine} alt="rose wine" className="categories__image" />
           </div>
-        </section>
-        <section className="categories">
-          <div className="categories__wrapper">
-            <div>
-              <img src={redWine} alt="red wine" className="categories__image" />
-              <img
-                src={whiteWine}
-                alt="white wine"
-                className="categories__image"
-              />
-              <img
-                src={sparklingWine}
-                alt="sparkling wine"
-                className="categories__image"
-              />
-              <img
-                src={roseWine}
-                alt="rose wine"
-                className="categories__image"
-              />
-            </div>
-          </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </main>
   );
 };
