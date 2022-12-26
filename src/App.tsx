eb2741eeb3875f22535928b4f7015982c15b66f0
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-26 13:10:43 +0300

    feat: run npm run lint -- --fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "./containers/Header/Header";
 import Footer from "./containers/Footer/Footer";
 import MainPage from "./pages/MainPage/MainPage";
-// import CataloguePage from "./pages/CataloguePage/CataloguePage";
-// import ProductCardPage from "./pages/ProductCardPage/ProductCardPage";
-// import CartPage from "./pages/CartPage/CartPage";
-// import Page404 from "./pages/Page404/Page404";
+import CataloguePage from "./pages/CataloguePage/CataloguePage";
+import ProductCardPage from "./pages/ProductCardPage/ProductCardPage";
+import CartPage from "./pages/CartPage/CartPage";
+import Page404 from "./pages/Page404/Page404";
 
 const App: React.FC = () => (
   <div className="App">
     <Header />
     <MainPage />
-    {/* <CataloguePage />
+    <CataloguePage />
     <ProductCardPage />
     <CartPage />
-    <Page404 /> */}
+    <Page404 />
     <Footer />
   </div>
 );
