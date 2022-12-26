10a8a8eb14113c5ff1ae454285236dea97f92d48
mitjasha <mitjasha@gmail.com>
2022-12-26 22:55:35 +0300

    refactor: change tags

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -4,9 +4,9 @@ import "./Page404.scss";
 
 const Page404: React.FC = () => {
   return (
-    <div className="page-404">
+    <main className="page-404">
       <div className="container page-404__container">
-        <h1 className="page-404__heading">404</h1>
+        <h2 className="page-404__heading">404</h2>
         <p className="page-404__description">
           Oops! The page can&apos;t be found
         </p>
@@ -21,7 +21,7 @@ const Page404: React.FC = () => {
           </Button>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 export default Page404;
