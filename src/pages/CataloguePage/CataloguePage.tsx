eeaa75b30d213fe3e90b43ecef0c173ef75fe994
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-12-29 16:26:08 +0300

    feat: add wrappers to catalogue page

--- src/pages/CataloguePage/CataloguePage.tsx
+++ src/pages/CataloguePage/CataloguePage.tsx
@@ -3,8 +3,16 @@ import "./CataloguePage.scss";
 
 const CataloguePage: React.FC = () => {
   return (
-    <div className="catalogue-page-container">
-      <div>CataloguePage</div>
+    <div className="catalogue">
+      <div className="container">
+        <div className="settings">1</div>
+        <div className="filters-goods-wrapper">
+          <div className="filters">
+            <input type="checkbox" className="filters__type" />
+          </div>
+          <div className="goods">1</div>
+        </div>
+      </div>
     </div>
   );
 };
