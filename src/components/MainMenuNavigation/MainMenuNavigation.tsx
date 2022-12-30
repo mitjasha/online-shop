a7ab4ecff20b7935c803c5477e8382e9a5be3de9
mitjasha <mitjasha@gmail.com>
2022-12-31 00:28:29 +0300

    feat: add main menu buttons

--- src/components/MainMenuNavigation/MainMenuNavigation.tsx
+++ src/components/MainMenuNavigation/MainMenuNavigation.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import Button from "../Buttons/Button/Button";
 import "./MainMenuNavigation.scss";
 
 const MainMenuNavigation: React.FC = () => {
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="list__item">HOME</li>
-        <li className="list__item">CATALOGUE</li>
-        <li className="list__item">CONTACTS</li>
-        <li className="list__item">CART</li>
+        <li className="list__item">
+          <Button to="/">HOME</Button>
+        </li>
+        <li className="list__item">
+          <Button to="/catalogue">CATALOGUE</Button>
+        </li>
+        <li className="list__item">
+          <Button to="/contacts">CONTACTS</Button>
+        </li>
+        <li className="list__item">
+          <Button to="/cart">CART</Button>
+        </li>
       </ul>
     </nav>
   );
