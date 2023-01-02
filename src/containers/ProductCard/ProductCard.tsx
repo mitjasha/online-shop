464f0df39d5ccfb0ec20e2b059d8f0d68e73f5e3
mitjasha <mitjasha@gmail.com>
2023-01-02 23:25:37 +0300

    refactor: add ProductCardActions component

--- src/containers/ProductCard/ProductCard.tsx
+++ src/containers/ProductCard/ProductCard.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import cn from "classnames";
 import RaitingStars from "../../components/RatingStars/RatingStars";
 import Button from "../../components/Buttons/Button/Button";
+import ProductCardActions from "../../components/ProductCardActions/ProductCardActions";
 import { WineInfo } from "../../utils/helpers/interfaces";
-import searchIcon from "../../assets/img/svg/search-icon.svg";
-import heartIcon from "../../assets/img/svg/heart-icon.svg";
-import cartIcon from "../../assets/img/svg/cart-icon.svg";
 import "./ProductCard.scss";
 
 interface ProductCardProps extends WineInfo {
@@ -27,25 +25,13 @@ const ProductCard: React.FC<ProductCardProps> = ({
         src={images ? images[1] : ""}
         alt="product"
       />
-      <div className="product-card__content">
-        <RaitingStars
-          className="product-card__raiting"
-          stars={rating === undefined ? 0 : rating}
-        />
-        <h3 className="product-card__title">{title}</h3>
-      </div>
+      <RaitingStars
+        className="product-card__raiting"
+        stars={rating === undefined ? 0 : rating}
+      />
+      <h3 className="product-card__title">{title}</h3>
       <Button className="product-card__price">{price}</Button>
-      <div className="product-card__actions">
-        <Button className="product-card__action-btn">
-          <img src={cartIcon} alt="cart" className="cart" />
-        </Button>
-        <Button className="product-card__action-btn">
-          <img src={heartIcon} alt="favorities" className="favorities" />
-        </Button>
-        <Button className="product-card__action-btn">
-          <img src={searchIcon} alt="modal" className="modal" />
-        </Button>
-      </div>
+      <ProductCardActions className="product-card__actions" />
     </div>
   );
 };
