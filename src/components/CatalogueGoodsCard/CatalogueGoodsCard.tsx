cf273a39dbe16e5c7910595626ab44869ac44bcf
mitjasha <mitjasha@gmail.com>
2023-01-04 01:23:51 +0300

    fix: link nesting problem

--- src/components/CatalogueGoodsCard/CatalogueGoodsCard.tsx
+++ src/components/CatalogueGoodsCard/CatalogueGoodsCard.tsx
@@ -21,15 +21,17 @@ const CatalogueGoodsCard: React.FC<CatalogueGoodsCardProps> = ({
 }) => {
   const classes = cn("goods-card", classname);
   return (
-    <Link className={classes} to={`/product/${id}`}>
-      <img
-        src={data.images?.[0]}
-        alt={data.title}
-        className="goods-card__image"
-      />
-      <h3 className="goods-card__title">{data.title}</h3>
-      <p className="goods-card__rating">Rating: ★{data.rating}</p>
-      <p className="goods-card__price">{data.price}</p>
+    <>
+      <Link className={classes} to={`/product/${id}`}>
+        <img
+          src={data.images?.[0]}
+          alt={data.title}
+          className="goods-card__image"
+        />
+        <h3 className="goods-card__title">{data.title}</h3>
+        <p className="goods-card__rating">Rating: ★{data.rating}</p>
+        <p className="goods-card__price">{data.price}</p>
+      </Link>
       <div className="goods-card__actions">
         <Button className="goods-card__action-btn">
           <img src={cartIcon} alt="cart" className="cart" />
@@ -41,7 +43,7 @@ const CatalogueGoodsCard: React.FC<CatalogueGoodsCardProps> = ({
           <img src={searchIcon} alt="modal" className="modal" />
         </Button>
       </div>
-    </Link>
+    </>
   );
 };
 
