36a99a44275ccc9951ad9288f0e70a64a22a7288
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-01-03 17:09:29 +0300

    feat: add sortItems function and fix data.json

--- src/pages/CataloguePage/CataloguePage.tsx
+++ src/pages/CataloguePage/CataloguePage.tsx
@@ -1,14 +1,35 @@
 import React from "react";
+import data from "../../assets/data/data.json";
 import CatalogueSettings from "../../components/CatalogueSettings/CatalogueSettings";
 import CatalogueFilters from "../../components/CatalogueFilters/CatalogueFilters";
 import CatalogueGoods from "../../components/CatalogueGoods/CatalogueGoods";
 import "./CataloguePage.scss";
 
 const CataloguePage: React.FC = () => {
+  let goodsArr = data.goods;
+  const sortItems = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.value === "priceDown") {
+      goodsArr = goodsArr.sort((a, b) => {
+        return Number(b.price.slice(1)) - Number(a.price.slice(1));
+      });
+    } else if (event.target.value === "priceUp") {
+      goodsArr = goodsArr.sort((a, b) => {
+        return Number(a.price.slice(1)) - Number(b.price.slice(1));
+      });
+    } else if (event.target.value === "ratingDown") {
+      goodsArr = goodsArr.sort((a, b) => {
+        return b.rating - a.rating;
+      });
+    } else if (event.target.value === "ratingUp") {
+      goodsArr = goodsArr.sort((a, b) => {
+        return a.rating - b.rating;
+      });
+    }
+  };
   return (
     <div className="catalogue">
       <div className="container">
-        <CatalogueSettings />
+        <CatalogueSettings sortFunction={sortItems} />
         <div className="filters-goods-wrapper">
           <CatalogueFilters />
           <CatalogueGoods />
