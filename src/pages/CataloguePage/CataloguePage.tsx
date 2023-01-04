1eb717790f2ffaae628d4c8083a5eb6273507d21
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-01-04 17:50:08 +0300

    feat: add useState hook

--- src/pages/CataloguePage/CataloguePage.tsx
+++ src/pages/CataloguePage/CataloguePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../../assets/data/data.json";
 import CatalogueSettings from "../../components/CatalogueSettings/CatalogueSettings";
 import CatalogueFilters from "../../components/CatalogueFilters/CatalogueFilters";
@@ -6,24 +6,29 @@ import CatalogueGoods from "../../containers/CatalogueGoods/CatalogueGoods";
 import "./CataloguePage.scss";
 
 const CataloguePage: React.FC = () => {
+  const [sortData, setSortData] = useState(data.goods);
   let goodsArr = data.goods;
   const sortItems = (event: React.ChangeEvent<HTMLSelectElement>) => {
     if (event.target.value === "priceDown") {
       goodsArr = goodsArr.sort((a, b) => {
         return Number(b.price.slice(1)) - Number(a.price.slice(1));
       });
+      setSortData(goodsArr);
     } else if (event.target.value === "priceUp") {
       goodsArr = goodsArr.sort((a, b) => {
         return Number(a.price.slice(1)) - Number(b.price.slice(1));
       });
+      setSortData(goodsArr);
     } else if (event.target.value === "ratingDown") {
       goodsArr = goodsArr.sort((a, b) => {
         return b.rating - a.rating;
       });
+      setSortData(goodsArr);
     } else if (event.target.value === "ratingUp") {
       goodsArr = goodsArr.sort((a, b) => {
         return a.rating - b.rating;
       });
+      setSortData(goodsArr);
     }
   };
   return (
@@ -32,7 +37,7 @@ const CataloguePage: React.FC = () => {
         <CatalogueSettings sortFunction={sortItems} />
         <div className="filters-goods-wrapper">
           <CatalogueFilters />
-          <CatalogueGoods data={data.goods} />
+          <CatalogueGoods data={sortData} />
           <div className="not-found">
             <p>No products found</p>
           </div>
