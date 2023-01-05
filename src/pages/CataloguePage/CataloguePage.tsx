e1052760fda8aa0d4ec4c559f20fae759a4cd32f
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-01-05 15:11:35 +0300

    feat: add filterCheckbox function and update sortItems function

--- src/pages/CataloguePage/CataloguePage.tsx
+++ src/pages/CataloguePage/CataloguePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import data from "../../assets/data/data.json";
 import CatalogueSettings from "../../components/CatalogueSettings/CatalogueSettings";
 import CatalogueFilters from "../../components/CatalogueFilters/CatalogueFilters";
@@ -6,52 +6,96 @@ import CatalogueGoods from "../../containers/CatalogueGoods/CatalogueGoods";
 import "./CataloguePage.scss";
 
 const CataloguePage: React.FC = () => {
-  const [sortData, setSortData] = useState(data.goods);
-  let goodsArr = [...data.goods];
-  const sortItems = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (event.target.value === "priceDown") {
-      goodsArr = goodsArr.sort((a, b) => {
-        return Number(b.price.slice(1)) - Number(a.price.slice(1));
+  const [sortData, setSortData] = useState({
+    products: data.goods,
+    filters: new Set(),
+  });
+
+  const sortItems = useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setSortData((previousState) => {
+        const filters = new Set(previousState.filters);
+        let products = [...data.goods];
+        if (filters.size) {
+          products = [...data.goods].filter((product) => {
+            return filters.has(product.type);
+          });
+          products = [...data.goods].filter((product) => {
+            return filters.has(product.title);
+          });
+        }
+        if (event.target.value === "priceDown") {
+          products.sort((a, b) => {
+            return Number(b.price.slice(1)) - Number(a.price.slice(1));
+          });
+        } else if (event.target.value === "priceUp") {
+          products.sort((a, b) => {
+            return Number(a.price.slice(1)) - Number(b.price.slice(1));
+          });
+        } else if (event.target.value === "ratingDown") {
+          products.sort((a, b) => {
+            return b.rating - a.rating;
+          });
+        } else if (event.target.value === "ratingUp") {
+          products.sort((a, b) => {
+            return a.rating - b.rating;
+          });
+        }
+        return {
+          filters,
+          products,
+        };
       });
-      setSortData(goodsArr);
-    } else if (event.target.value === "priceUp") {
-      goodsArr = goodsArr.sort((a, b) => {
-        return Number(a.price.slice(1)) - Number(b.price.slice(1));
+    },
+    [setSortData],
+  );
+
+  const filterCheckbox = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const typesArr = [
+        "Red wine",
+        "White wine",
+        "Rosé wine",
+        "Sparkling wine",
+      ];
+      setSortData((previousState) => {
+        const filters = new Set(previousState.filters);
+        let products = [...data.goods];
+
+        if (event.target.checked) {
+          filters.add(event.target.id);
+        } else {
+          filters.delete(event.target.id);
+        }
+
+        if (filters.size && typesArr.includes(event.target.id)) {
+          products = products.filter((product) => {
+            return filters.has(product.type);
+          });
+        } else if (filters.size) {
+          products = products.filter((product) => {
+            return filters.has(product.title);
+          });
+        }
+        return {
+          filters,
+          products,
+        };
       });
-      setSortData(goodsArr);
-    } else if (event.target.value === "ratingDown") {
-      goodsArr = goodsArr.sort((a, b) => {
-        return b.rating - a.rating;
-      });
-      setSortData(goodsArr);
-    } else if (event.target.value === "ratingUp") {
-      goodsArr = goodsArr.sort((a, b) => {
-        return a.rating - b.rating;
-      });
-      setSortData(goodsArr);
-    } else if (event.target.value === "default") {
-      setSortData(goodsArr);
-    }
-  };
-  const filterCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const typesArr = ["Red wine", "White wine", "Rosé wine", "Sparkling wine"];
-    if (event.target.checked && typesArr.includes(event.target.id)) {
-      goodsArr = goodsArr.filter((elem) => elem.type === event.target.id);
-    } else if (event.target.checked) {
-      goodsArr = goodsArr.filter((elem) => elem.title === event.target.id);
-    }
-    return setSortData(goodsArr);
-  };
+    },
+    [setSortData],
+  );
+
   return (
     <div className="catalogue">
       <div className="container">
         <CatalogueSettings
           sortFunction={sortItems}
-          itemsFound={sortData.length}
+          itemsFound={sortData.products.length}
         />
         <div className="filters-goods-wrapper">
           <CatalogueFilters filterFunction={filterCheckbox} />
-          <CatalogueGoods data={sortData} />
+          <CatalogueGoods data={sortData.products} />
           <div className="not-found">
             <p>No products found</p>
           </div>
