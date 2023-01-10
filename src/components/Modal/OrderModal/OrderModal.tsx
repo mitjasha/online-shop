609c6ca7ee9f738c0ae293c16795da3ca4781786
mitjasha <mitjasha@gmail.com>
2023-01-11 01:03:41 +0300

    fix: submit function

--- src/components/Modal/OrderModal/OrderModal.tsx
+++ src/components/Modal/OrderModal/OrderModal.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 import {
   validationAddress,
   validationCardCVV,
@@ -37,16 +38,19 @@ interface PersonalData {
 
 const OrderModal: React.FC<OrderModalProps> = ({ isVisible, setIsVisible }) => {
   const [cardNumber, setCardNumber] = useState<string>("");
+  const [isConfirm, setIsConfirm] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const {
     register,
     formState: { errors },
     handleSubmit,
-    reset,
+    clearErrors,
   } = useForm<PersonalData>({ mode: "onChange" });
 
   const onSubmit = () => {
-    reset();
+    setIsConfirm(true);
+    setTimeout(() => navigate("/"), 3000);
   };
 
   return (
@@ -55,130 +59,149 @@ const OrderModal: React.FC<OrderModalProps> = ({ isVisible, setIsVisible }) => {
       setIsVisible={setIsVisible}
       header="place an order"
     >
-      <form className="order-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="order-form__container">
-          <div className="person-details">
-            <h2 className="person-details__title">Personal details </h2>
-            <div className="person-details__item">
-              <InputForm
-                type="text"
-                placeholder="Name"
-                name="personName"
-                className="person-details__name form-item"
-                register={register("name", { ...validationName })}
-              />
-              {errors?.name && <ToolTip text={validationName.message} />}
-            </div>
-
-            <div className="person-details__item">
-              <InputForm
-                type="tel"
-                placeholder="Phone number"
-                name="phoneNumber"
-                className="person-details__phone-number form-item"
-                register={register("phone", { ...validationPhone })}
-              />
-              {errors?.phone && <ToolTip text={validationPhone.message} />}
-            </div>
-
-            <div className="person-details__item">
-              <InputForm
-                type="text"
-                placeholder="Delivery address"
-                name="address"
-                className="person-details__address form-item"
-                register={register("address", { ...validationAddress })}
-              />
-              {errors?.address && <ToolTip text={validationAddress.message} />}
-            </div>
-
-            <div className="person-details__item">
-              <InputForm
-                type="text"
-                placeholder="E-mail"
-                name="email"
-                className="person-details__email form-item"
-                register={register("email", { ...validationEmail })}
-              />
-              {errors?.email && <ToolTip text={validationEmail.message} />}
-            </div>
-          </div>
-
-          <div className="card-details">
-            <h2 className="person-details__title">Credit card details</h2>
-
-            <div className="card-data">
-              <img className="card-image" alt="card" src={cardPNG} />
-              <div className="card-number">
+      {!isConfirm && (
+        <form className="order-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="order-form__container">
+            <div className="person-details">
+              <h2 className="person-details__title">Personal details </h2>
+              <div className="person-details__item">
                 <InputForm
                   type="text"
-                  name="cardNumber"
-                  placeholder="Card number"
-                  className="card-number__input"
-                  register={register("cardNumber", { ...validationCardNumber })}
-                  onchange={(evt) => {
-                    setCardNumber(evt.target.value);
-                  }}
-                />
-                {errors?.cardNumber && (
-                  <ToolTip text={validationCardNumber.message} />
-                )}
-              </div>
-
-              <div className="card-date">
-                <InputForm
-                  type="text"
-                  name="cardDate"
-                  placeholder="Date"
-                  className="card-date__input"
-                  register={register("cardDate", { ...validationCardDate })}
-                />
-                {errors?.cardDate && (
-                  <ToolTip text={validationCardDate.message} />
-                )}
-              </div>
-
-              <img
-                className="card-payment"
-                alt="card"
-                src={
-                  cardNumber.slice()[0] === "4"
-                    ? visa
-                    : cardNumber.slice()[0] === "5"
-                    ? master
-                    : maestro
-                }
-              />
-
-              <div className="card-name">
-                <InputForm
-                  type="text"
-                  placeholder="Cardholder Name"
+                  placeholder="Name"
                   name="personName"
-                  className="card-name__input"
-                  register={register("cardName", { ...validationName })}
+                  className="person-details__name form-item"
+                  register={register("name", { ...validationName })}
+                  onchange={() => clearErrors("name")}
                 />
-                {errors?.cardName && <ToolTip text={validationName.message} />}
+                {errors?.name && <ToolTip text={validationName.message} />}
               </div>
 
-              <div className="card-cvv">
+              <div className="person-details__item">
+                <InputForm
+                  type="tel"
+                  placeholder="Phone number"
+                  name="phoneNumber"
+                  className="person-details__phone-number form-item"
+                  register={register("phone", { ...validationPhone })}
+                  onchange={() => clearErrors("phone")}
+                />
+                {errors?.phone && <ToolTip text={validationPhone.message} />}
+              </div>
+
+              <div className="person-details__item">
                 <InputForm
                   type="text"
-                  name="cardCVV"
-                  placeholder="CVV"
-                  className="card-cvv__input"
-                  register={register("cardCvv", { ...validationCardCVV })}
+                  placeholder="Delivery address"
+                  name="address"
+                  className="person-details__address form-item"
+                  register={register("address", { ...validationAddress })}
+                  onchange={() => clearErrors("address")}
                 />
-                {errors?.cardCvv && (
-                  <ToolTip text={validationCardCVV.message} />
+                {errors?.address && (
+                  <ToolTip text={validationAddress.message} />
                 )}
+              </div>
+
+              <div className="person-details__item">
+                <InputForm
+                  type="text"
+                  placeholder="E-mail"
+                  name="email"
+                  className="person-details__email form-item"
+                  register={register("email", { ...validationEmail })}
+                  onchange={() => clearErrors("email")}
+                />
+                {errors?.email && <ToolTip text={validationEmail.message} />}
+              </div>
+            </div>
+
+            <div className="card-details">
+              <h2 className="person-details__title">Credit card details</h2>
+
+              <div className="card-data">
+                <img className="card-image" alt="card" src={cardPNG} />
+                <div className="card-number">
+                  <InputForm
+                    type="text"
+                    name="cardNumber"
+                    placeholder="Card number"
+                    className="card-number__input"
+                    register={register("cardNumber", {
+                      ...validationCardNumber,
+                    })}
+                    onchange={(evt) => {
+                      setCardNumber(evt.target.value);
+                      clearErrors("cardNumber");
+                    }}
+                  />
+                  {errors?.cardNumber && (
+                    <ToolTip text={validationCardNumber.message} />
+                  )}
+                </div>
+
+                <div className="card-date">
+                  <InputForm
+                    type="text"
+                    name="cardDate"
+                    placeholder="Date"
+                    className="card-date__input"
+                    register={register("cardDate", { ...validationCardDate })}
+                    onchange={() => clearErrors("cardDate")}
+                  />
+                  {errors?.cardDate && (
+                    <ToolTip text={validationCardDate.message} />
+                  )}
+                </div>
+
+                <img
+                  className="card-payment"
+                  alt="card"
+                  src={
+                    cardNumber.slice()[0] === "4"
+                      ? visa
+                      : cardNumber.slice()[0] === "5"
+                      ? master
+                      : maestro
+                  }
+                />
+
+                <div className="card-name">
+                  <InputForm
+                    type="text"
+                    placeholder="Cardholder Name"
+                    name="personName"
+                    className="card-name__input"
+                    register={register("cardName", { ...validationName })}
+                    onchange={() => clearErrors("cardName")}
+                  />
+                  {errors?.cardName && (
+                    <ToolTip text={validationName.message} />
+                  )}
+                </div>
+
+                <div className="card-cvv">
+                  <InputForm
+                    type="text"
+                    name="cardCVV"
+                    placeholder="CVV"
+                    className="card-cvv__input"
+                    register={register("cardCvv", { ...validationCardCVV })}
+                    onchange={() => clearErrors("cardCvv")}
+                  />
+                  {errors?.cardCvv && (
+                    <ToolTip text={validationCardCVV.message} />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <button type="submit">CONFIRM</button>
-      </form>
+          <button type="submit">CONFIRM</button>
+        </form>
+      )}
+      {isConfirm && (
+        <div className="order-form__container">The order has been placed</div>
+      )}
     </BaseModalComponent>
   );
 };
